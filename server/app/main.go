e877ea5661f0d7ae3e98260f87b8ed2430209555
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-05-01 18:23:49 +0900

    collect function

--- server/app/main.go
+++ server/app/main.go
@@ -21,17 +21,17 @@ type Template struct {
 }
 
 type User struct {
-	SerialNumber string `json:"serialNumber"`
-	Name string `json:"name"`
-	TimeStanp time.Time `json:"timeStanp"`
+	SerialNumber string    `json:"serialNumber"`
+	Name         string    `json:"name"`
+	TimeStanp    time.Time `json:"timeStanp"`
 }
 
 type Attendance struct {
-	Id        int       `json:"id"`
-	Name      string    `json:"name"`
-	TimeStart time.Time `json:"timeStart"`
-	TimeGoal  time.Time `json:"timeGoal"`
-	SerialNumber     []string    `json:"serialNumber"`
+	Id           int       `json:"id"`
+	Name         string    `json:"name"`
+	TimeStart    time.Time `json:"timeStart"`
+	TimeGoal     time.Time `json:"timeGoal"`
+	SerialNumber []string  `json:"serialNumber"`
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
@@ -40,7 +40,8 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 
 func main() {
 	fmt.Print("\n\n")
-	createDB()
+	createTestDB()
+	insertRecodAttendancdDB(2, "serialNumber0")
 	t := &Template{
 		templates: template.Must(template.ParseGlob("views/*/*.html")),
 	}
@@ -72,13 +73,13 @@ func hello(c echo.Context) error {
 
 func nfcTouchPOST(c echo.Context) error {
 	/*
-	// json形式ではなく、form形式で送信することにする
-	POST /nfc/touch HTTP/1.1
-	Host: localhost:1234
-	Content-Type: application/x-www-form-urlencoded
-	Content-Length: 14
+		// json形式ではなく、form形式で送信することにする
+		POST /nfc/touch HTTP/1.1
+		Host: localhost:1234
+		Content-Type: application/x-www-form-urlencoded
+		Content-Length: 14
 
-	serialNumber=3
+		serialNumber=3
 	*/
 
 	serialNumber := c.FormValue("serialNumber")
@@ -92,15 +93,14 @@ func nfcTouchPOST(c echo.Context) error {
 
 	// 受付中の出席管理があれば出席にする
 	for _, attendance := range *attendances {
-		if  time.Now().After(attendance.TimeStart) && time.Now().Before(attendance.TimeGoal) {
+		if time.Now().After(attendance.TimeStart) && time.Now().Before(attendance.TimeGoal) {
 			// 受付中
 			insertRecodAttendancdDB(attendance.Id, serialNumber)
 		}
 	}
-	
+
 	return c.JSON(http.StatusOK, user)
 }
-
 
 func attendanceNewGET(c echo.Context) error {
 	attendances := loadAttendanceDB()
@@ -175,14 +175,13 @@ func loadUsersDB() *[]User {
 	}
 
 	/*
-	sort.Slice(users, func(i, j int) bool {
-		return attendances[i].Id > attendances[j].Id
-	})
+		sort.Slice(users, func(i, j int) bool {
+			return attendances[i].Id > attendances[j].Id
+		})
 	*/
 
 	return &users
 }
-
 
 func loadAttendanceDB() *[]Attendance {
 	file, err := os.Open("./db/attendances.json")
@@ -209,83 +208,69 @@ func insertDB(attendance Attendance) int {
 	attendance.Id = (*attendances)[0].Id + 1
 	*attendances = append(*attendances, attendance)
 
-	file, err := os.Create("./db/attendances.json")
-	if err != nil {
-		return -1
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(attendances); err != nil {
-		return -1
-	}
+	createDB("./db/attendances.json", *attendances)
 	return attendance.Id
 }
 
-func insertRecodAttendancdDB(attendanceId int, serialNumber string)  {
+func insertRecodAttendancdDB(attendanceId int, serialNumber string) {
 	attendances := loadAttendanceDB()
-	for _, attendance := range *attendances{
+	for i, attendance := range *attendances {
 		if attendance.Id == attendanceId {
-			attendance.SerialNumber = append(attendance.SerialNumber, serialNumber)
+			(*attendances)[i].SerialNumber = append(attendance.SerialNumber, serialNumber)
 		}
 	}
 
-	file, err := os.Create("./db/attendances.json")
-	if err != nil {
-		return
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(attendances); err != nil {
-		return
-	}
-	return
+	createDB("./db/attendances.json", *attendances)
 }
 
-func createDB() {
-	users := []User{
+func createTestDB() {
+	var users interface{}
+	users = []User{
 		{
 			SerialNumber: "0",
-			Name: "kashi",
+			Name:         "kashi",
 		},
 		{
 			SerialNumber: "1",
-			Name: "sotaro",
+			Name:         "sotaro",
 		},
 		{
 			SerialNumber: "2",
-			Name: "sok",
+			Name:         "sok",
 		},
 	}
+	createDB("./db/users.json", users)
 
-	attendances := []Attendance{
+	var attendances interface{}
+	attendances = []Attendance{
 		{
-			Id:        0,
-			Name:      "点呼",
-			TimeStart: time.Date(2024, 04, 29, 23, 8, 2, 0, &time.Location{}),
-			TimeGoal:  time.Date(2024, 04, 29, 24, 8, 2, 0, &time.Location{}),
-			SerialNumber: 		[]string{"1", "0"},
+			Id:           0,
+			Name:         "点呼",
+			TimeStart:    time.Date(2024, 04, 29, 23, 8, 2, 0, &time.Location{}),
+			TimeGoal:     time.Date(2024, 04, 29, 24, 8, 2, 0, &time.Location{}),
+			SerialNumber: []string{"1", "0"},
 		},
 		{
-			Id:        1,
-			Name:      "コンピュータ部出席確認",
-			TimeStart: time.Date(2024, 04, 30, 23, 8, 2, 0, &time.Location{}),
-			TimeGoal:  time.Date(2024, 04, 30, 24, 8, 2, 0, &time.Location{}),
-			SerialNumber: 		[]string{"1", "2"},
+			Id:           1,
+			Name:         "コンピュータ部出席確認",
+			TimeStart:    time.Date(2024, 04, 30, 23, 8, 2, 0, &time.Location{}),
+			TimeGoal:     time.Date(2024, 04, 30, 24, 8, 2, 0, &time.Location{}),
+			SerialNumber: []string{"1", "2"},
 		},
 		{
-			Id:        2,
-			Name:      "点呼",
-			TimeStart: time.Date(2024, 05, 01, 0, 0, 0, 0, &time.Location{}),
-			TimeGoal:  time.Date(2024, 05, 01, 23, 8, 2, 0, &time.Location{}),
-			SerialNumber: 		[]string{"1", "2"},
+			Id:           2,
+			Name:         "点呼",
+			TimeStart:    time.Date(2024, 05, 01, 0, 0, 0, 0, &time.Location{}),
+			TimeGoal:     time.Date(2024, 05, 05, 23, 8, 2, 0, &time.Location{}),
+			SerialNumber: []string{"1", "2"},
 		},
 	}
+	createDB("./db/attendances.json", attendances)
+	return
+}
 
-	file, err := os.Create("./db/attendances.json")
+func createDB(DBName string, data interface{}) {
+	file, err := os.Create(DBName)
 	if err != nil {
 		return
 	}
@@ -293,21 +278,7 @@ func createDB() {
 
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(attendances); err != nil {
+	if err := encoder.Encode(data); err != nil {
 		return
 	}
-
-	file, err = os.Create("./db/users.json")
-	if err != nil {
-		return
-	}
-	defer file.Close()
-
-	encoder = json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(users); err != nil {
-		return
-	}
-
-	return
 }
