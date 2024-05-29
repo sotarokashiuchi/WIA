199b3202890cdd9ffd3dd6b2e8f26feb70329741
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-05-29 15:59:48 +0900

    create tone class

---
+++ RPiApp/tone.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+class Tone:
+    # Initialize an instance by specifying PWM pin number
+    def __init__(self, pwm_pin):
+        self.pwm = GPIO.PWM(pwm_pin, 0)
+ 
+    # Beep a tone by specifying frequency and duty
+    def beep(self, freq, duty=0.5):
+        self.pwm.ChangeFrequency(freq)
+        self.pwm.start(duty*100)
+    
+    # Beep a tone by specifying frequency and duty until duration time
+    def beepUntilTime(self, freq, duration, duty=0.5):
+        self.beep(freq, duty)
+        time.sleep(duration)
+        self.none()
+ 
+    def none(self):
+        self.pwm.ChangeDutyCycle(0)
+                                 
+    # Destroy instance by cleaning up pins
+    def destroy(self):
+        self.none()
+        GPIO.cleanup()
